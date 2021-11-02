44af8a8fd951e769375e534d70f752c6ff7c5e57
hardwin27 <hardwin27@gmail.com>
2021-11-02 10:38:19 +0800

    init commit

---
+++ Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveAccel;
+    public float maxSpeed;
+
+    private Rigidbody2D rig;
+
+    [Header("Jump")]
+    public float jumpAccel;
+
+    private bool isJumpuing;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+    
+    private bool isOnGround;
+
+    private Animator anim;
+
+    private CharacterSoundController sound;
+
+    [Header("Scoring")]
+    public ScoreController score;
+    public float scoringRatio;
+    private float lastPositionX;
+
+    [Header("GameOver")]
+    public float fallPositionY;
+    public GameObject gameOverScreen;
+
+    [Header("Camera")]
+    public CameraMoveController gameCamera;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sound = GetComponent<CharacterSoundController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumpuing = true;
+                sound.PlayJump();
+            }
+        }
+
+
+        //Update animation based on isOnGround
+        anim.SetBool("isOnGround", isOnGround);
+
+        //Scoring
+        int distancePassed = Mathf.FloorToInt(transform.position.x - lastPositionX);
+        int scoreIncrement = Mathf.FloorToInt(distancePassed / scoringRatio);
+
+        if(scoreIncrement > 0)
+        {
+            score.IncreaseCurrentScore(scoreIncrement);
+            lastPositionX += distancePassed;
+        }
+
+        //When game over
+        if(transform.position.y < fallPositionY)
+        {
+            GameOver();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Raycast to detect ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+
+        if(hit)
+        {
+            if(!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+
+        //Take velocity, modify, give it back
+        Vector2 velocityVector = rig.velocity;
+
+        if(isJumpuing)
+        {
+            velocityVector.y += jumpAccel;
+            isJumpuing = false;
+        }
+
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+        rig.velocity = velocityVector;
+    }
+
+    private void GameOver()
+    {
+        score.FinishScoring();
+
+        gameCamera.enabled = false;
+
+        gameOverScreen.SetActive(true);
+
+        this.enabled = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
+    }
+
+
+}
